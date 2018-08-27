c01c917aa245111994a20a9802ef7766a263930f
zhengdongge <banggzd@hotmail.com>
2018-08-27 15:59:26 -0400

    0827

---
+++ py/883_ Projection Area of 3D Shapes.py
@@ -0,0 +1,34 @@
+class Solution:
+    def projectionArea(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        h = 0
+        f = 0
+        r = 0
+        for i in range(0, len(grid)):
+            for j in range(0, len(grid[0])):
+                if grid[i][j] != 0:
+                    h += 1
+            f += max(grid[i])
+        for j in range(0, len(grid[0])):
+            maxt = grid[0][j]
+            for i in range(1, len(grid)):
+                if grid[i][j] > maxt:
+                    maxt = grid[i][j]
+            r += maxt
+
+        return (h + f + r)
+
+
+t = Solution()
+print(t.projectionArea([[1,2],[3,4]]))
+print(t.projectionArea([[1,1,1],[1,0,1],[1,1,1]]))
+print(t.projectionArea([[2,2,2],[2,1,2],[2,2,2]]))
+
+
+
+
+
+

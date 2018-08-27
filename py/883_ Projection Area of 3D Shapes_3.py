19eb6778fd4249ed922941a943b6dc75d3432a3b
zhengdongge <banggzd@hotmail.com>
2018-08-27 17:33:53 -0400

    0827

---
+++ py/883_ Projection Area of 3D Shapes_3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def projectionArea(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+
+
+
+t = Solution()
+print(t.projectionArea([[1,2],[3,4]]))
+print(t.projectionArea([[1,1,1],[1,0,1],[1,1,1]]))
+print(t.projectionArea([[2,2,2],[2,1,2],[2,2,2]]))
+
+
+
+
+
+

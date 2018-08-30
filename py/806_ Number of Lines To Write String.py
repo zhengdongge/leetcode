552478674744e9a5e1faf77537ae34f3f89d75cd
zhengdongge <banggzd@hotmail.com>
2018-08-29 22:17:14 -0400

    0829

--- py/806_ Number of Lines To Write String.py
+++ py/806_ Number of Lines To Write String.py
@@ -5,3 +5,13 @@ class Solution:
         :type S: str
         :rtype: List[int]
         """
+        lines = 1
+        linecharcount = 0
+        for char in S:
+            if widths[ord(char) - ord('a')] + linecharcount <= 100:
+                linecharcount += widths[ord(char) - ord('a')]
+            else:
+                lines +=1
+                linecharcount = widths[ord(char) - ord('a')]
+        return [lines, linecharcount]
+

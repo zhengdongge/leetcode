e6447b374d99cd6fc940d3ae8ba7398c48db4cdf
zhengdongge <banggzd@hotmail.com>
2018-09-05 14:46:19 -0400

    0905

--- py/896_Monotonic Array.py
+++ py/896_Monotonic Array.py
@@ -7,20 +7,24 @@ class Solution:
         if len(A) == 1:
             return True
         s = 1
-        while A[s-1] == A[s]:
+        while A[s-1] == A[s] and s < len(A) - 1:
             s += 1
         if s == len(A) - 1:
             return True
         elif A[s-1] < A[s]:
-            i = s + 1
-            while i < len(A) - 1:
+            i = s
+            while i <= len(A) - 1:
                 if A[i-1] > A[i]:
                     return False
                 i += 1
-        elif A[0] > A[1]:
-            i = s + 1
-            while i < len(A) - 1:
+        elif A[s-1] > A[s]:
+            i = s
+            while i <= len(A) - 1:
                 if A[i-1] < A[i]:
                     return False
                 i += 1
         return True
+
+
+t = Solution()
+print(t.isMonotonic([1,3,2]))

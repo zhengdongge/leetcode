0e7259d55c4182e4ff122d85d465acd2380c8e68
zhengdongge <banggzd@hotmail.com>
2018-09-14 16:54:06 -0400

    0914

--- py/811_ Subdomain Visit Count.py
+++ py/811_ Subdomain Visit Count.py
@@ -4,4 +4,20 @@ class Solution:
         :type cpdomains: List[str]
         :rtype: List[str]
         """
-        
+        ans = {}
+        reans = []
+        for domain in cpdomains:
+            times, url = domain.split(' ')
+            urls = url.split('.')
+            for i in range(len(urls)):
+                if '.'.join(urls[i:]) not in ans:
+                    ans['.'.join(urls[i:])] = int(times)
+                else:
+                    ans['.'.join(urls[i:])] += int(times)
+
+        for u, t in ans.items():
+            reans.append(str(t) + ' ' + u)
+        return reans
+
+t = Solution()
+print(t.subdomainVisits(["9001 discuss.leetcode.com"]))
